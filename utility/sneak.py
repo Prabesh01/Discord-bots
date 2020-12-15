6c40cccd98e96e3809e878b0be95c727c542dfdc
Prabesh Sapkota <prabesh01@pm.me>
2020-12-15 13:58:57 +0000

    Create sneak.py

---
+++ utility/sneak.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+c = discord.Client()
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+@c.event
+async def on_ready():
+    pass     
+
+@c.event
+async def on_message(message):
+    print('Sender: '+str(message.author))
+    clts = message.channel.id
+    channel = c.get_channel(clts)
+    print('Channel: ',channel)
+    ID = message.guild.id
+    guild = c.get_guild(ID)
+    print('Server: '+str(guild))    
+    print('Message:'+str(message.content)+"\n")
+    
+c.run(DISCORD_TOKEN)
