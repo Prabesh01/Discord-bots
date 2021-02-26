c42f9c80c8a52dfff67691f53c90b373b250b300
Prabesh Sapkota <prabesh01@pm.me>
2021-02-26 17:24:57 +0545

    Update youtubey.py

--- youtubey.py
+++ youtubey.py
@@ -35,15 +35,19 @@ headers = {
 response = requests.post('https://www.youtube.com//youtubei/v1/notification/get_notification_menu?key='+apikey, headers=headers, cookies=cookies, json={"context":{"client":{"clientName":"WEB","clientVersion":"2.20210224.06.00",},},"notificationsMenuRequestType":"NOTIFICATIONS_MENU_REQUEST_TYPE_INBOX"})
 out=response.text
 out=json.loads(out)
-for i in range(20):
-    vid=out['actions'][0]['openPopupAction']['popup']['multiPageMenuRenderer']['sections'][0]['multiPageMenuNotificationSectionRenderer']['items'][i]['notificationRenderer']['navigationEndpoint']['watchEndpoint']['videoId']
-    fl('y.txt')
-    f = open("y.txt", "r")
-    prev=f.read()
-    old=re.search(vid, prev)
-    if old==None: 
-        webhook = DiscordWebhook(username='Youtubey', avatar_url='https://yt3.ggpht.com/ytc/AAUvwnjF2lmkwh5hSWpkpEckEunyx1KHTC9rdWvign77vg=s88-c-k-c0x00ffffff-no-rj', url=wurl, content="https://www.youtube.com/watch?v="+vid)
+try:
+    for i in range(20):
+        vid=out['actions'][0]['openPopupAction']['popup']['multiPageMenuRenderer']['sections'][0]['multiPageMenuNotificationSectionRenderer']['items'][i]['notificationRenderer']['navigationEndpoint']['watchEndpoint']['videoId']
+        fl('y.txt')
+        f = open("y.txt", "r")
+        prev=f.read()
+        old=re.search(vid, prev)
+        if old==None: 
+            webhook = DiscordWebhook(username='Youtubey', avatar_url='https://yt3.ggpht.com/ytc/AAUvwnjF2lmkwh5hSWpkpEckEunyx1KHTC9rdWvign77vg=s88-c-k-c0x00ffffff-no-rj', url=wurl, content="https://www.youtube.com/watch?v="+vid)
+            response = webhook.execute()
+            f = open("y.txt", "a")
+            f.write(vid+'\n')
+            f.close()
+    except Exception as e:
+        webhook = DiscordWebhook(username='Youtubey', avatar_url='https://yt3.ggpht.com/ytc/AAUvwnjF2lmkwh5hSWpkpEckEunyx1KHTC9rdWvign77vg=s88-c-k-c0x00ffffff-no-rj', url=wurl, content="Something went wrong. Most probably, the provided values might have expired. Error:"+str(e))
         response = webhook.execute()
-        f = open("y.txt", "a")
-        f.write(vid+'\n')
-        f.close()
