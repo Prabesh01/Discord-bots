ecf4dcdc6a3c31dfcd49c888dc791999b93a3d90
Prabesh Sapkota <prabesh01@pm.me>
2021-02-27 17:59:16 +0545

    Update hook.py

--- hook.py
+++ hook.py
@@ -29,8 +29,15 @@ def routiney():
         old=re.search(time, prev)
         if old==None: 
             webhook = DiscordWebhook(username='Routiney', avatar_url='https://pbs.twimg.com/profile_images/777188003445739521/t5GNGfAc_400x400.jpg', url=hooks)
-            embed = DiscordEmbed(description=str(post['text']))
-            embed.set_image(url=post['image'])
+            embed = DiscordEmbed(description=str(post['text']))            
+            embed.set_author(name='Routiney', url=post['post_url'], icon_url='https://pbs.twimg.com/profile_images/777188003445739521/t5GNGfAc_400x400.jpg')
+            img = post['images']
+            try:
+                embed.set_image(url=img[1])
+                embed.set_thumbnail(url=img[0])
+            except:
+                embed.set_image(url=post['image'])
+            embed.set_footer(text='Blah Blah Blah ...') 
             webhook.add_embed(embed)
             response = webhook.execute()
             print(response)
