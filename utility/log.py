0a246a92c4db856b769d546a9d765b8eaab4f820
Prabesh Sapkota <prabesh01@pm.me>
2021-04-02 17:41:56 +0545

    Create log.py

---
+++ utility/log.py
@@ -0,0 +1,37 @@
+import discord
+from os import path
+#import logging
+import pandas as pd
+#logging.basicConfig(level=logging.INFO)
+
+c = discord.Client()
+
+@c.event
+async def on_message(message):
+    if message.author == c.user:
+        return  
+    clts = message.channel.id
+    channel = c.get_channel(clts)
+    ID = message.guild.id
+    guild = c.get_guild(ID)
+    data = pd.DataFrame(columns=['time', 'author','guild','channel','content'])
+    if path.exists(str(ID)+'.csv') !=True:
+        async for msg in message.channel.history(limit=10000000000000000000000000):
+            data = data.append({'time': msg.created_at.isoformat(),
+                                'author': msg.author.name,
+                                'guild': guild,
+                                'channel': channel,
+                                'content': msg.content}, ignore_index=True)
+        data=data.iloc[::-1]
+        file_location = str(ID)+".csv"
+        data.to_csv(file_location, index=False)
+        #print('done')
+    else:
+        data = [
+            [message.created_at.isoformat(),message.author.name, guild, channel,  message.content]
+        ]
+        dataframe = pd.DataFrame(data)
+        dataframe.to_csv(str(ID)+".csv", index=False, mode='a', header=False)
+        #print('dups')
+    
+c.run('TOKEN_HERE')
