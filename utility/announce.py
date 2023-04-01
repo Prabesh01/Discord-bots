a40652f6a33220ab486eedab5d691e91dda84c55
Prabesh Sapkota <pspsapkota2059@gmail.com>
2023-04-01 11:48:27 +0545

    Update announce.py

--- utility/announce.py
+++ utility/announce.py
@@ -6,11 +6,15 @@ blob = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx"
 r=requests.get(blob, timeout=20).json()
 hooks=r['hooks']
 
-webhook = DiscordWebhook(url=hooks, username="Routiney",avatar_url="https://i.imgur.com/tQOuMXT.png",rate_limit_retry=True)
+#webhook = DiscordWebhook(url=hooks, username="Routiney",avatar_url="https://i.imgur.com/tQOuMXT.png",rate_limit_retry=True)
+webhooks = DiscordWebhook.create_batch(urls=hooks, username="Routiney",avatar_url="https://i.imgur.com/tQOuMXT.png",rate_limit_retry=True)
 
-embed = DiscordEmbed(description='**stuffs:** and more stuffs ',color='e14044')
-embed.set_author(name='Routine of Nepal banda', url='https://facebook.com/officialroutineofnepalbanda/posts/XXXXXXXXXXXXXXXX', icon_url='https://i.ibb.co/jvY8NK2/ronb.png')
-embed.set_image(url='https://i.ibb.co/XXXXXXX/Capture.png')
+embed = DiscordEmbed(description='**Breaking News: Nepal Government to Ban Discord!** \nThe government of Nepal has announced that it will soon be imposing a ban on the popular communication platform, Discord. According to the concerned authority, Government is taking this action after discord denied to comply with national policies regarding tax and social media regulations. Some officials have also expressed concern that Discord is being used for pornographic contents and cyberbullying which raises moral concerns. ||April 1st mfker||',color='e14044')
+embed.set_author(name='Routine of Nepal banda', url='https://www.facebook.com/114269511553283_152326627762641', icon_url='https://i.ibb.co/jvY8NK2/ronb.png')
+embed.set_image(url='https://i.ibb.co/qgmvkr3/dcban.jpg')
 
-webhook.add_embed(embed)
-response = webhook.execute()
+ilen=len(webhooks)
+for i, webhook in enumerate(webhooks, start=1):
+    print(f"{i}/{ilen}")
+    webhook.add_embed(embed)
+    response = webhook.execute()
